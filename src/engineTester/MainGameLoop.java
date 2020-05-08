c14bdce56e955edf3df29ff240fbbb04ce1967a0
daria25D <dasha_domracheva@mail.ru>
2020-05-08 22:25:14 +0300

    Add cube instead of quad, enable depth test

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -24,31 +24,94 @@ public class MainGameLoop {
         Renderer renderer = new Renderer(shader);
 
         float[] vertices = {
-                -0.5f, 0.5f, 0f,
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0.5f, 0.5f, 0f
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,-0.5f,0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                0.5f,0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,-0.5f,
+                -0.5f,-0.5f,-0.5f,
+                -0.5f,-0.5f,0.5f,
+                -0.5f,0.5f,0.5f,
+
+                -0.5f,0.5f,0.5f,
+                -0.5f,0.5f,-0.5f,
+                0.5f,0.5f,-0.5f,
+                0.5f,0.5f,0.5f,
+
+                -0.5f,-0.5f,0.5f,
+                -0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,-0.5f,
+                0.5f,-0.5f,0.5f
+
         };
+
         float[] uv = {
-                0f, 0f,
-                0f, 1f,
-                1f, 1f,
-                1f, 0f
+
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
+
+
         };
+
         int[] indices = {
-                0, 1, 3, 3, 1, 2
+                0,1,3,
+                3,1,2,
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
+
         };
 
         RawModel model = loader.loadToVAO(vertices, uv, indices);
         ModelTexture texture = new ModelTexture(loader.loadTexture("tex1"));
         TexturedModel staticModel = new TexturedModel(model, texture);
-        Entity entity = new Entity(staticModel, new Vector3f(0, 0, 0), 0, 0, 0, 1);
+        Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
 
         Camera camera = new Camera();
 
         while(!Display.isCloseRequested()) {
-            entity.increasePosition(0, 0, -0.02f);
-//            entity.increaseRotation(2, 0, 0);
+//            entity.increasePosition(0, 0, -0.02f);
+            entity.increaseRotation(1, 1, 0);
             camera.move();
             renderer.prepare();
             shader.start();
